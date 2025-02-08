745bb9bc6fc91267fd64f5e78cf845088fc8cd75
ettabet aicha <aicha70tabite@gmail.com>
2025-02-08 23:34:16 +0100

    applying the state at the config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'; // ✅ Import properly
+
+import { CollectionService } from './core/services/collection.service';
+import { routes } from './app.routes';
+import { collectionReducer } from './state/reducers/collectionReq.reducer';
+import { CollectionEffects } from './state/effects/collectionReq.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     provideHttpClient(),
+    CollectionService,
+    provideStore({ collection: collectionReducer }),
+    provideEffects(CollectionEffects),
+    importProvidersFrom(StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })), // ✅ Fix DevTools
+    provideRouter(routes),
     provideAnimations()
   ]
 };
