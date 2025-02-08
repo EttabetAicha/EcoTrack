686fa72f58ff3b5cd09d2e3bbc8c40781418439b
ettabet aicha <aicha70tabite@gmail.com>
2025-02-08 23:33:03 +0100

    adding state selectors

---
+++ src/app/state/selectors/collectionReq.selectors.ts
@@ -0,0 +1,23 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { CollectionState } from '../reducers/collectionReq.reducer';
+
+export const selectCollectionState = createFeatureSelector<CollectionState>('collection');
+
+export const selectCollectionRequests = createSelector(
+  selectCollectionState,
+  (state: CollectionState) => state.requests
+);
+
+export const selectCollectionError = createSelector(
+  selectCollectionState,
+  (state: CollectionState) => state.error
+);
+
+export const selectPendingRequests = createSelector(
+  selectCollectionRequests,
+  (requests) => requests.filter(request => request.status === 'pending')
+);
+export const selectRequestsByCity = (city: string) => createSelector(
+  selectCollectionRequests,
+  (requests) => requests.filter(request => request.address.city === city)
+);
